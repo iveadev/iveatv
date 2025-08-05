b188a01f0a55f9623933cbef89cbc0bef894a638
Angel Aguirre <angelaguirrel@hotmail.com>
2025-08-05 10:51:24 -0600

    🩹 Se corrige el obtener el rango de fechas de un evento

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -42,7 +42,7 @@ class Event extends Model
         $from = date_create($this->visibleFrom);
         $dif = $diff=date_diff($from,date_create($this->visibleTo));
         $range = [date_format($from, $format)];
-        for ($i=0; $i < $dif->d ; $i++) { 
+        for ($i=0; $i < $dif->days ; $i++) { 
             date_add($from, date_interval_create_from_date_string('1 day'));
             array_push($range,date_format($from, $format));
         }
